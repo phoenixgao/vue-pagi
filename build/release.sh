ab84546fe71a5d713cb2ebf1d07021f77b240dae
Xiang Gao <phoenix.x.gao@gmail.com>
2016-10-24 18:28:25 +0800

    change to webpack

---
+++ build/release.sh
@@ -0,0 +1,22 @@
+set -e
+echo "Enter release version: "
+read VERSION
+
+read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
+echo    # move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo "Releasing $VERSION ..."
+  yarn test
+  VERSION=$VERSION yarn run build
+
+  # commit
+  #git add -A
+  #git commit -m "[build] $VERSION"
+  #yarn version --new-version $VERSION
+
+  # publish
+  #git push origin refs/tags/v$VERSION
+  #git push
+  #yarn publish
+fi
