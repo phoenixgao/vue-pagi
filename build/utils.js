e1d40a90020720eb833140c257780ad6ec7a5862
Xiang Gao <phoenix.x.gao@gmail.com>
2016-10-26 21:59:34 +0800

    who knoes

--- build/utils.js
+++ build/utils.js
@@ -3,10 +3,11 @@ var config = require('../config')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 exports.assetsPath = function (_path) {
-  var assetsSubDirectory = process.env.NODE_ENV === 'production'
-    ? config.build.assetsSubDirectory
-    : config.dev.assetsSubDirectory
-  return path.posix.join(assetsSubDirectory, _path)
+  return _path
+  // var assetsSubDirectory = process.env.NODE_ENV === 'production'
+  //   ? config.build.assetsSubDirectory
+  //   : config.dev.assetsSubDirectory
+  // return path.posix.join(assetsSubDirectory, _path)
 }
 
 exports.cssLoaders = function (options) {
