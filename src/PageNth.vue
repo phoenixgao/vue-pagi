28d57201f9ba7f4ab81ee7125db963ee55c605d2
Xiang Gao <phoenix.x.gao@gmail.com>
2016-10-30 21:58:59 +0800

    commit

--- src/PageNth.vue
+++ src/PageNth.vue
@@ -1,16 +1,22 @@
 <template>
-  <li v-class:class='{"active":current}'>
-    <a v-show="type === 'link'" v-on:click.prevent="changePage(page.text)">{{ page.text }}</a>
-    <a v-show="type === 'ellipsis'">{{ page.text }}</a>
+  <li v-bind:class='{"active":current}'>
+    <a v-show="type === 'link'" v-on:click.prevent="changePage(text)">{{ text }}</a>
+    <a v-show="type === 'ellipsis'">{{ text }}</a>
   </li>
 </template>
 
 <script>
 export default {
   props: {
-    current: false,
-    text: '',
-    type: 'link'
+    current: Boolean,
+    text: [String, Number],
+    type: String,
+    onclick: Function
+  },
+  methods: {
+    changePage: function (page) {
+      this.onclick(page)
+    }
   }
 }
 </script>
